7b180f403e73cf6e6ce8e3172c9c9f4294d5c4f7
machine <analyticrye@gmail.com>
2020-05-17 01:06:57 -0400

    clean up and NotFound component added

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import Search from "../users/Search";
+import Users from "../users/Users";
+const Home = () => {
+  return (
+    <div>
+      <Fragment>
+        <Search />
+        <Users />
+      </Fragment>
+    </div>
+  );
+};
+export default Home;
